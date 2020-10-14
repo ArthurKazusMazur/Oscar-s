8373a8298db3e3a73a8cda685a843f4bfdf5b7b5
Artur Mazur <Artur.Mazur@itechart-group.com>
2020-10-14 19:12:05 +0300

    parametrize and Xfail are added

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,12 +1,24 @@
+import pytest
+import time
+
 from pages.product_page import PromoPage
 from pages.locators import PromoPageLocators
-from pages.locators import BasketPageLocators
-
-url = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+from pages.locators import ConfirmationPageLocators
 
 
-def test_guest_can_add_product_to_basket(browser):
-    page = PromoPage(browser, url)
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  pytest.param("bugged_link", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    browser.delete_all_cookies()
+    page = PromoPage(browser, link)
     page.open()
 
     catalogue_product_name = browser.find_element(*PromoPageLocators.CATALOGUE_PRODUCT_NAME).text
@@ -14,10 +26,9 @@ def test_guest_can_add_product_to_basket(browser):
 
     page.add_to_basket()
     page.solve_quiz_and_get_code()
-    page.view_basket()
 
-    basket_product_name = browser.find_element(*BasketPageLocators.BASKET_PRODUCT_NAME).text
-    basket_product_price = browser.find_element(*BasketPageLocators.BASKET_TOTAL_PRICE).text
+    basket_product_name = browser.find_element(*ConfirmationPageLocators.BASKET_PRODUCT_NAME).text
+    basket_product_price = browser.find_element(*ConfirmationPageLocators.BASKET_TOTAL_PRICE).text
 
     assert catalogue_product_price == basket_product_price, "PRODUCT PRICE DOES NOT MATCH!!!"
     assert catalogue_product_name == basket_product_name, "PRODUCT NAME DOES NOT MATCH!!!"
