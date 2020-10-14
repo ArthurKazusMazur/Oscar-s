b3e64559f3977860779f2f627439d5687dffedbf
Artur Mazur <Artur.Mazur@itechart-group.com>
2020-10-14 17:16:58 +0300

    product page object

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,9 +1,19 @@
-from .base_page import BasePage
-from .locators import PromoPageLocators
+from pages.base_page import BasePage
+from pages.locators import PromoPageLocators
 
 
 class PromoPage(BasePage):
     def add_to_basket(self):
-        self.browser.find_element(*PromoPageLocators.ADD_TO_BASKET_BTN)\
+        self.browser.find_element(*PromoPageLocators.ADD_TO_BASKET_BTN) \
             .click()
-        self.browser.solve_quiz_and_get_code()
+
+    def view_basket(self):
+        self.browser.find_element(*PromoPageLocators.VIEW_BASKET_BTN).click()
+
+    def get_catalogue_product_price(self):
+
+        catalogue_product_price = self.browser.find_element(*PromoPageLocators.CATALOGUE_PRODUCT_NAME).text
+        print(catalogue_product_price.text)
+
+    def get_catalogue_product_name(self):
+        pass
