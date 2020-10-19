2812cf93fc415d63112ceed2b9016c6c9aae110c
Artur Mazur <Artur.Mazur@itechart-group.com>
2020-10-19 18:13:26 +0300

    test ran configuration

--- conftest.py
+++ conftest.py
@@ -9,7 +9,7 @@ def pytest_addoption(parser):
     parser.addoption('--language', action="store", default='en', help="Choose your language: ru, en, es, fr, etc...")
 
 
-@pytest.fixture()
+@pytest.fixture
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     user_language = request.config.getoption("language")
@@ -19,18 +19,21 @@ def browser(request):
         print("\n\nStarting GOOGLE Chrome browser for test...")
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
-        # options.add_argument('headless')  # эта опция позволяет запускать браузер "в безголовом" режиме
+        options.add_argument('headless')  # эта опция позволяет запускать браузер "в безголовом" режиме
         browser = webdriver.Chrome(options=options)
         browser.maximize_window()
+        browser.delete_all_cookies()
 
     elif browser_name == "firefox":
         print("\n\nStarting Mazila Firefox browser for test...")
         fp = webdriver.FirefoxProfile()
         fp.set_preference("intl.accept_languages", user_language)
         browser = webdriver.Firefox(firefox_profile=fp)
+        browser.maximize_window()
+        browser.delete_all_cookies()
     else:
         raise pytest.UsageError("--browser_name should be 'chrome' or 'firefox'")
 
     yield browser
     print("\n\nQuit browser")
-    # browser.quit()
+    browser.quit()
