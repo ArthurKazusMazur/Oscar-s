fe881b7a5200da12f93f5c944dd564cbaadfd388
Artur Mazur <Artur.Mazur@itechart-group.com>
2020-10-20 12:35:57 +0300

    page locators are described here

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,34 @@
 from selenium.webdriver.common.by import By
 
 
-class PromoPageLocators():
+class MainPageLocators():
+    VIEW_BASKET_BTN = (By.XPATH, '//*[@id="default"]/header/div[1]//div[2]/span/a')
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+    USER_ICON = (By.CSS_SELECTOR, '.icon-user')
+
+
+class LoginPageLocators():
+    REGISTER_EMAIL_INPUT = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD_INPUT = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONF_PASSWORD_INPUT = (By.CSS_SELECTOR, "#id_registration-password2")
+    CONFIRM_REGISTER_BTN = (By.CSS_SELECTOR, '[name="registration_submit"]')
+    LOGIN_EMAIL_INPUT = (By.CSS_SELECTOR, '#id_login-username')
+    LOGIN_PASSWORD_INPUT = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BTN = (By.CSS_SELECTOR, '[name="login_submit"]')
+    CONFIRMATION_MESSAGE = (By.CSS_SELECTOR, '.alertinner.wicon')
+
+
+class ProductPageLocators():
     ADD_TO_BASKET_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")
     VIEW_BASKET_BTN = (By.XPATH, '//p/a[1][@class = "btn btn-info"]')
     CATALOGUE_PRODUCT_NAME = (By.XPATH, '//div/h1')
     CATALOGUE_PRICE = (By.XPATH, '//div/p[@class="price_color"]')
 
 
+class ConfirmationPageLocators():
+    BASKET_TOTAL_PRICE = (By.XPATH, '//div[@class="alertinner "]/p[1]/strong')
+    BASKET_PRODUCT_NAME = (By.XPATH, '//div[@class="alertinner " and text()]/strong')
+
+
 class BasketPageLocators():
-    BASKET_TOTAL_PRICE = (By.XPATH, '//h3[@class="price_color"]')
-    BASKET_PRODUCT_NAME = (By.XPATH, '//div//h3/a')
+    EMPTY_BASKET_MESSAGE = (By.XPATH, '//div/p')
