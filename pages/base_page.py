59aedf1b1eb0713a1b582a3945161c2aef2c4f4b
Artur Mazur <Artur.Mazur@itechart-group.com>
2020-10-20 12:34:21 +0300

    class BasePage and common metods

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,11 @@
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoAlertPresentException, TimeoutException
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import math
+
+from pages.locators import MainPageLocators
 
 
 class BasePage():
@@ -12,9 +17,39 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, how, what, timeout=5):
         try:
-            self.browser.find_element(how, what)
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except NoSuchElementException:
+            return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
+    def go_to_login_page(self, how, what, reg_url):
+        try:
+            self.browser.find_element(how, what).click()
+            WebDriverWait(self.browser, 5).until(EC.url_to_be(reg_url))
+        except TimeoutException:
+            return False
+        return True
+
+    def go_to_basket_page(self, how, what):
+        try:
+            self.browser.find_element(how, what) \
+                .click()
         except NoSuchElementException:
             return False
         return True
@@ -33,4 +68,6 @@ class BasePage():
         except NoAlertPresentException:
             print("No second alert presented")
 
-
+    def is_user_authorized(self):
+        assert self.is_element_present(*MainPageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
