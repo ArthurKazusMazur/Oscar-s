ae4baeebb80b7960c8af881a45cbe47f5d732c1e
Artur Mazur <Artur.Mazur@itechart-group.com>
2020-10-20 12:39:11 +0300

    product page object

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,19 +1,20 @@
 from pages.base_page import BasePage
-from pages.locators import PromoPageLocators
+from pages.locators import ProductPageLocators
+from pages.locators import ConfirmationPageLocators
 
 
-class PromoPage(BasePage):
+class ProductPage(BasePage):
     def add_to_basket(self):
-        self.browser.find_element(*PromoPageLocators.ADD_TO_BASKET_BTN) \
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BTN) \
             .click()
 
-    def view_basket(self):
-        self.browser.find_element(*PromoPageLocators.VIEW_BASKET_BTN).click()
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ConfirmationPageLocators.BASKET_PRODUCT_NAME), \
+            "CONFIRMATION MESSAGE is PRESENT"
 
-    def get_catalogue_product_price(self):
-
-        catalogue_product_price = self.browser.find_element(*PromoPageLocators.CATALOGUE_PRODUCT_NAME).text
-        print(catalogue_product_price.text)
+    def should_message_disappear(self):
+        assert self.is_disappeared(*ConfirmationPageLocators.BASKET_PRODUCT_NAME), \
+            "CONFIRMATION MESSAGE did not DISAPPEAR!!!"
 
     def get_catalogue_product_name(self):
         pass
